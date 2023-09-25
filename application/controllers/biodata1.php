3a7cfa0c4850901a75ec11a833191134e5d94c1e
ILALIQBAL <iqbalilal92@gmail.com>
2023-09-25 04:25:21 -0700

    menambah pustaka-booking

---
+++ application/controllers/biodata1.php
@@ -0,0 +1,14 @@
+<?php 
+class biodata1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "nama saya ilal iqbal
+              nim :19220707
+              saya tinggal dijambi
+              hobi yang saya 
+              sukai adalah playstation";
+
+    }        
+}
